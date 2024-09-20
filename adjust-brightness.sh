f8638760d844e3a76754d92b9599be2e35ba9a3b
Abhijeeth Babu <121789736+abhijeeth-babu@users.noreply.github.com>
2024-09-20 17:16:45 +0530

    Update adjust-brightness.sh

--- adjust-brightness.sh
+++ adjust-brightness.sh
@@ -1,27 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Save this script to: /usr/lib/systemd/system-sleep/adjust-brightness.sh
+# Add your commands here
+# echo "System has woken up from suspend at $(date)" >> /tmp/wake-up-log.txt
 
 brightness_file="/sys/class/backlight/amdgpu_bl1/brightness"
-saved_brightness_file="/tmp/saved_brightness"
-
-case "$1" in
-  pre)
-    # Save the current brightness before suspend
-    current_brightness=$(cat "$brightness_file")
-    echo "$current_brightness" > "$saved_brightness_file"
-  ;;
-  post)
-    # Wait for 2 seconds after resume
-    sleep 2
-
-    # Restore the brightness after resume
-    if [ -f "$saved_brightness_file" ]; then
-      previous_brightness=$(cat "$saved_brightness_file")
-      echo "$previous_brightness" | tee "$brightness_file"
-    else
-      # Set the brightness to 35 if no saved brightness is found
-      echo 35 | tee "$brightness_file"
-    fi
-  ;;
-esac
+# You can add more commands as needed
+sleep 0.33
+cat "$brightness_file" | tee "$brightness_file"
+# echo "Resetting brightness" >> /tmp/wake-up-log.txt
